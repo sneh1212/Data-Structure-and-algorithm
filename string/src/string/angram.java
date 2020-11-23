ff93c4614f696562c18853ba216c05a8a7eb7733
sneh1212 <kumarsnehshahu@gmail.com>
2020-11-23 12:36:06 +0530

    1

--- string/src/string/angram.java
+++ string/src/string/angram.java
@@ -10,16 +10,43 @@ public class angram {
 		
 		char c1[]=input1.toCharArray();
 		char c2[]=input2.toCharArray();
-		Arrays.sort(c1);
-		Arrays.sort(c2);
-		input1=new String(c1);
-		input2=new String(c2);
-		if(input1.equals(input2))
-			System.out.println(true);
+//		Arrays.sort(c1);
+//		Arrays.sort(c2);
+//		input1=new String(c1);
+//		input2=new String(c2);
+//		if(input1.equals(input2))
+//			System.out.println(true);
+//		else
+//			System.out.println(false);
+		
+		//optimization
+		if(c1.length==c2.length)
+		{
+		int freq[] = new int[26];
+		for(char i:c1)
+			freq[i-'a']++;
+		for(char i:c2)
+			freq[i-'a']--;
+		int check=0;
+		for(int i:freq)
+		{
+			if(i>0)
+			{
+				
+					check++;
+					break;
+			}
+			
+		}
+		if(check==0)
+		{
+			System.out.println("true");
+		}
 		else
-			System.out.println(false);
-		
-		
+			System.out.println("false");
+		}
+		else
+			System.out.println("false");
 
 	}
 
