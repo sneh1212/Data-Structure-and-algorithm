041a5d4b368187232ad2c83e98d4ef6e05756c29
sneh1212 <kumarsnehshahu@gmail.com>
2020-11-23 12:28:09 +0530

    2

--- string/src/string/angram.java
+++ string/src/string/angram.java
@@ -1,9 +1,25 @@
 package string;
-
+import java.util.*;
 public class angram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		String input1=s.next();
+		String input2=s.next();
+		
+		char c1[]=input1.toCharArray();
+		char c2[]=input2.toCharArray();
+		Arrays.sort(c1);
+		Arrays.sort(c2);
+		input1=new String(c1);
+		input2=new String(c2);
+		if(input1.equals(input2))
+			System.out.println(true);
+		else
+			System.out.println(false);
+		
+		
 
 	}
 
