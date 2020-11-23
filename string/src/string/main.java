da01f8a684ee850d451620991c9b0e28e6b33e96
sneh1212 <31516571+sneh1212@users.noreply.github.com>
2020-11-23 20:26:12 +0530

    Excel Sheet Column Number

--- string/src/string/main.java
+++ string/src/string/main.java
@@ -1,5 +1,50 @@
 package string;
+/*
+ * Excel Sheet Column Number
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
 
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+Example 1:
+
+Input: "A"
+Output: 1
+Example 2:
+
+Input: "AB"
+Output: 28
+Example 3:
+
+Input: "ZY"
+Output: 701
+ 
+
+Constraints:
+
+1 <= s.length <= 7
+s consists only of uppercase English letters.
+s is between "A" and "FXSHRXW"
+ */
 public class main {
+	
+public int titleToNumber(String A) {
+        
+        int res=0;
+        for(int i=0;i<A.length();i++)
+        {
+            int temp=(int)Math.pow(26,i)+((A.charAt(i)-'A'));
+            res+=temp;
+        }
+        return res;
+        
+    }
 
 }
